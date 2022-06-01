ef33dd00504ff15f7f204a336f61a768f687656b
Victorhck <victorhck@mailbox.org>
2022-06-01 17:19:31 +0200

    Update IPbot.py

--- IPbot.py
+++ IPbot.py
@@ -2,14 +2,14 @@ import requests
 import os
 
 URL = "https://api.telegram.org/"
-token = "bot..."
+token = "bot....."
 method = "sendMessage"
-chatID = 
+chatID = 1111111111
 folder = os.getcwd()+os.sep+".IP"
 
 def checkIP():
     result = []
-    newIP = str(requests.get("http://ifconfig.me/").text)
+    newIP = str(requests.get("https://ifconfig.me/").text)
 
     if not os.path.exists(folder):
         os.mkdir(folder)
@@ -19,7 +19,7 @@ def checkIP():
             oldIP = file.read().replace("\n","")
             file.close()
 
-        print("old IP: "+oldIP+"\nnewIP: "+newIP)
+        print("IP antigua: "+oldIP+"\nIP nueva:   "+newIP)
 
         if newIP != oldIP:
             os.remove(folder+os.sep+"IP.txt")
@@ -43,14 +43,14 @@ def checkIP():
     return result
 
 def sendmessage(IP):
-    data = {'chat_id': chatID, 'text': 'Home IP has changed,\nNew IP is '+str(IP)}
+    data = {'chat_id': chatID, 'text': 'La IP ha cambiado 🔄\nLa nueva IP es: '+str(IP)}
     print(data)
     r = requests.post(URL+token+"/"+method, data).json()
     print(r)
 
 def main():
     IP_has_Changed = checkIP()
-    print("[has changed, IP]: "+str(IP_has_Changed))
+    print("¿Ha cambiado la IP? "+str(IP_has_Changed))
 
     if IP_has_Changed[0]:
         sendmessage(IP_has_Changed[1])
